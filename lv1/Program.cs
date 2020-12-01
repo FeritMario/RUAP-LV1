3d0c9698d13f92b5a831ed4e56191806fcfaf290
Student <student@iMac-15.local>
2020-12-01 18:49:48 +0100

    First commit

---
+++ lv1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lv1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Antonia Novinc");
+        }
+    }
+}
